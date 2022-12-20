9b5cab03688e328aed7e00b5316d0a2bef1f0467
Matthew Evans <git@ml-evs.science>
2022-12-20 23:58:50 +0100

    Placeholder

--- src/atomate2/lammps/flows/core.py
+++ src/atomate2/lammps/flows/core.py
@@ -3,18 +3,20 @@ from dataclasses import dataclass, field
 from jobflow import Flow, Maker
 from pymatgen.core import Structure
 
-from atomate2.lammps.sets.base import BaseLammpsGenerator, LammpsMinimization
-
 from ..jobs.base import LammpsMaker
-from ..jobs.core import MinimizationMaker
+from ..jobs.core import MeltMaker, QuenchMaker
 
 
 @dataclass
-class FMinimizationMaker(Maker):
+class MeltQuenchThermalizeMaker(Maker):
     # potentially remove and replace with single job
-    name: str = "minimization"
-    minimization_maker: LammpsMaker = field(default_factory=MinimizationMaker)
-    input_set_generator: BaseLammpsGenerator = field(default_factory=LammpsMinimization)
+    name: str = "melt-quench-thermalize"
+    melt_maker: LammpsMaker = field(default_factory=MeltMaker)
+    quench_maker: LammpsMaker = field(default_factory=QuenchMaker)
+    thermalize_maker: LammpsMaker = field(default_factory=ThermalizationMaker)
 
     def make(self, structure: Structure):
-        return Flow([self.minimization_maker.make(structure)], name=self.name)
+        melt = self.melt_maker.make(structure)
+        quench = self.quench_maker.make(melt.output.structure)
+        thermalize = self.thermalize_make.make(quench.output.structure)
+        return Flow([melt, quench, thermalize], name=self.name)
--- src/atomate2/lammps/jobs/core.py
+++ src/atomate2/lammps/jobs/core.py
@@ -1,8 +1,52 @@
+import abc
 from dataclasses import dataclass, field
+from typing import Callable, Union
 
-from atomate2.lammps.sets.base import BaseLammpsGenerator, LammpsMinimization
+from pymatgen.core import Structure
+
+from atomate2.lammps.sets.base import (
+    BaseLammpsGenerator,
+    LammpsMelt,
+    LammpsMinimization,
+    LammpsQuench,
+    LammpsThermalize,
+)
 
 from ..jobs.base import BaseLammpsMaker
+
+
+class StoppingCriteria(abc.ABC):
+    """Abstract class for encapsulating criteria for ending a LAMMPS dynamics run."""
+
+
+class AutocorrelationCriteria(StoppingCriteria):
+    pass
+
+
+@dataclass
+class MeltMaker(BaseLammpsMaker):
+    melt_temperature: float
+    stopping_criteria: str | Callable[
+        Union[Structure, List[Structure]]
+    ] | StoppingCriteria
+    name: str = "melt"
+    input_set_generator: BaseLammpsGenerator = field(default_factory=LammpsMelt)
+
+
+@dataclass
+class ThermalizeMaker(BaseLammpsMaker):
+    thermalization_temperature: float
+    stopping_criteria: str | Callable[
+        Union[Structure, List[Structure]]
+    ] | StoppingCriteria
+    name: str = "thermalize"
+    input_set_generator: BaseLammpsGenerator = field(default_factory=LammpsThermalize)
+
+
+@dataclass
+class QuenchMaker(BaseLammpsMaker):
+    name: str = "quench"
+    input_set_generator: BaseLammpsGenerator = field(default_factory=LammpsQuench)
 
 
 @dataclass
